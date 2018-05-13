6f179b9f0b0b80495439679e5d1808e160f7ca22
Alex Peralta <aperalta@salesforce.com>
2018-05-13 14:34:41 -0400

    First Commit

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+//create array of default friends
+
+var friends = [{
+    name: "Joe Doe",
+    photo: "https://images.pexels.com/photos/6945/sunset-summer-golden-hour-paul-filitchkin.jpg",
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+}, {
+    name: "John Smith",
+    photo: "https://images.pexels.com/photos/732629/pexels-photo-732629.jpeg",
+    scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+}, {
+    name: "Luis Miller",
+    photo: "https://images.pexels.com/photos/697675/pexels-photo-697675.jpeg",
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+},{
+    name: "Bill Thorn",
+    photo: "https://images.pexels.com/photos/804172/pexels-photo-804172.jpeg",
+    scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+}, {
+    name: "Mike Trout",
+    photo: "https://images.pexels.com/photos/301952/pexels-photo-301952.jpeg",
+    scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+}, {
+    name: "Bob Miller",
+    photo: "https://images.pexels.com/photos/551852/pexels-photo-551852.jpeg",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+}, {
+    name: "Tom Smith",
+    photo: "https://images.pexels.com/photos/943241/pexels-photo-943241.jpeg",
+    scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+}];
+
+//export array
+
+module.exports = friends;
